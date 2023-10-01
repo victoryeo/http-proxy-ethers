e6bf30c1604423cb5adcd8cfb3c87d03198f9160
yeosiowvic <victoryeo1974@gmail.com>
2023-10-01 11:23:34 +0800

    update proxy server

--- extras/proxy-server.js
+++ extras/proxy-server.js
@@ -4,26 +4,39 @@ const express = require("express");
 const { ethers } = require("ethers");
 
 // Create an instance of the proxy server
-const proxy = httpProxy.createProxyServer();
-
-// Create a basic HTTP server
-const server = http.createServer((req, res) => {
-  // Log the request
-  console.log(`Proxying request for: ${req.url}`);
-
-  // Proxy the request to the target server
-  proxy.web(req, res, {
-    changeOrigin: true,
-    secure: false,
-    target: "https://ocbc.tokenmint.eu/rpc/mumbai", // Replace with the target server's URL
-  });
-});
+const proxy = httpProxy.createProxyServer({})
 
 // Handle errors
 proxy.on("error", (err, req, res) => {
   console.error("Proxy error:", err);
   res.statusCode = 500;
   res.end("Proxy Error");
+});
+
+proxy.on("proxyReq", function(proxyReq, req, res, options) {
+  console.log(
+    `Proxy connection from ${
+      socket.remoteAddress
+    } with url: ${JSON.stringify(req.url)}`
+  );
+  proxyReq.setHeader('X-Special-Proxy-Header', 'ocbc');
+});
+
+// Create a target HTTP server
+const server = http.createServer((req, res) => {
+  // Log the request
+  console.log(`Proxying request for: ${req.url}`);
+  // Proxy the request to the target server
+  proxy.web(req, res, {
+    changeOrigin: true,
+    secure: false,
+    target: "https://ocbc.tokenmint.eu/rpc/mumbai", // Replace with the target server's URL
+  });
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + 
+    JSON.stringify(req.headers, true, 2));
+  res.end();
 });
 
 // Set the proxy server to listen on a specific port
@@ -40,20 +53,3 @@ server.on("connect", (res, socket) => {
     } with headers: ${JSON.stringify(res.rawHeaders)}`
   );
 });
-
-// // server.get("/", (req, res) => {
-// //   res.send({ message: "Hello API" });
-// // });
-
-// const app = express();
-
-// // Define a route for your API endpoint
-// app.get("/api/resource", (req, res) => {
-//   const provider = new ethers.JsonRpcProvider(
-//     "https://ocbc.tokenmint.eu/rpc/mumbai"
-//   );
-
-//   console.log("console provider", provider);
-
-//   res.json({ message: "This is your API response." });
-// });
